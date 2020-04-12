65949d60937652b9f8cfd1c4497dabe50d1cfa0c
宮崎脩平 <miyazakishuhei@miyazakiosamutairanoMacBook-Pro.local>
2020-04-12 11:17:11 +0900

    テスト

--- spotify.php
+++ spotify.php
@@ -2,7 +2,7 @@
 
 require_once('vendor/autoload.php');
     $session = new SpotifyWebAPI\Session(
-        "${ENV['CLIENT_ID']}",
+        $ENV['CLIENT_ID'],
         '1e08120c75754a8f8de076cf79de318b'
     );
     $api = new SpotifyWebAPI\SpotifyWebAPI();
