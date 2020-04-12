23e018ed717675873c48095de160e939c0f1e5da
宮崎脩平 <miyazakishuhei@miyazakiosamutairanoMacBook-Pro.local>
2020-04-12 11:29:05 +0900

    テスト

--- spotify.php
+++ spotify.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once('vendor/autoload.php');
-    $session = new SpotifyWebAPI\Session(
-        $ENV['CLIENT_ID'],
-        '1e08120c75754a8f8de076cf79de318b'
-    );
-    $api = new SpotifyWebAPI\SpotifyWebAPI();
-    $session->requestCredentialsToken();
-    $accessToken = $session->getAccessToken();
-    $api->setAccessToken($accessToken);
+$CLIENT_ID = getenv('CLIENT_ID');
+$session = new SpotifyWebAPI\Session(
+    "{$CLIENT_ID}",
+    '1e08120c75754a8f8de076cf79de318b'
+);
+$api = new SpotifyWebAPI\SpotifyWebAPI();
+$session->requestCredentialsToken();
+$accessToken = $session->getAccessToken();
+$api->setAccessToken($accessToken);
